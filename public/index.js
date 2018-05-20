da01f95badb97a97eea5a3db3df4407b5854f7d6
Beaglefoot <ch-e-st@yandex.ru>
2018-05-20 20:18:00 +0300

    Promisify the process of image retrieval

--- public/index.js
+++ public/index.js
@@ -1,13 +1,20 @@
 {
-  const images = new Array(8).fill().map((_, i) => `./public/lit${i}.svg`);
+  const images = new Array(8).fill().map(
+    (_, i) =>
+      new Promise(resolve => {
+        const image = new Image();
+        image.src = `./public/lit${i}.svg`;
+        image.onload = () => resolve(image);
+      })
+  );
 
-  console.log(images);
+  Promise.all(images).then(images => {
+    const img = document.getElementsByTagName('img')[0];
+    let counter = images.length - 1;
 
-  const img = document.getElementsByTagName('img')[0];
-  let counter = images.length - 1;
-
-  setInterval(() => {
-    counter = counter - 1 || images.length - 1;
-    img.src = images[counter];
-  }, 100);
+    setInterval(() => {
+      counter = counter - 1 || images.length - 1;
+      img.src = images[counter].src;
+    }, 100);
+  });
 }
